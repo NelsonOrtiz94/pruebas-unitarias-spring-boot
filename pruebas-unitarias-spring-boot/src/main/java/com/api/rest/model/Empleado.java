0e4f62359f7b4a9a84d01b4ea7229c6bdd8ed97c
NelsonOrtiz94 <ortnn990@gmail.com>
2025-07-01 10:14:55 -0500

    Se estructura Exception para guardar Empleado

--- pruebas-unitarias-spring-boot/src/main/java/com/api/rest/model/Empleado.java
+++ pruebas-unitarias-spring-boot/src/main/java/com/api/rest/model/Empleado.java
@@ -1,6 +1,13 @@
 package com.api.rest.model;
 
 import jakarta.persistence.*;
+import lombok.*;
+
+@Setter
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
 
 @Entity
 @Table(name = "empleados")
@@ -10,7 +17,12 @@ public class Empleado {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
 
+    @Column(name = "nombre", nullable = false)
     private String nombre;
+
+    @Column(name = "apellido", nullable = false)
     private String apellido;
+
+    @Column(name = "email", nullable = false)
     private String email;
 }
