b7a986a9e5eeefc748023a3679e43bb58cab4b07
NelsonOrtiz94 <ortnn990@gmail.com>
2025-07-03 10:20:54 -0500

    Test para listar empleados

--- pruebas-unitarias-spring-boot/src/test/java/com/api/rest/repository/EmpleadoRepositoryTests.java
+++ pruebas-unitarias-spring-boot/src/test/java/com/api/rest/repository/EmpleadoRepositoryTests.java
@@ -3,9 +3,14 @@ package com.api.rest.repository;
 import static org.assertj.core.api.Assertions.assertThat;
 
 import com.api.rest.model.Empleado;
+import org.aspectj.lang.annotation.Before;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+
+import java.util.List;
 
 // This annotation is used to configure a test that focuses on JPA components
 @DataJpaTest
@@ -14,6 +19,18 @@ public class EmpleadoRepositoryTests {
     @Autowired
     private EmpleadoRepository empleadoRepository;
 
+    private Empleado empleado;
+
+    @BeforeEach
+    void setUp() {
+        empleado = Empleado.builder()
+                .nombre("Empleado 1")
+                .apellido("Apellido 1")
+                .email("c1@gmail.com")
+                .build();
+    }
+
+    @DisplayName("Test para guardar un empleado")
     @Test
     void testGuardarEmpleado(){
         // BDD (Behavior Driven Development) - Se centra en el comportamiento esperado del sistema
@@ -31,6 +48,27 @@ public class EmpleadoRepositoryTests {
         // Then - Se espera que el empleado se guarde correctamente
         assertThat(empleadoGuardado).isNotNull();
         assertThat(empleadoGuardado.getId()).isGreaterThan(0);
+    }
+
+    @DisplayName("Test para listar empelados")
+    @Test
+    void testListarEmpleados() {
+        // Given - Se crean y guardan varios empleados
+        Empleado empleado1 = Empleado.builder()
+                .nombre("Julen")
+                .apellido("Oliva")
+                .email("j12@gmail.com")
+                .build();
+
+        empleadoRepository.save(empleado1);
+        empleadoRepository.save(empleado);
+
+        // When - Se guardan los empleados en la base de datos
+        List<Empleado> listaEmpleados = empleadoRepository.findAll();
+
+        // Then - Se espera que la lista de empleados no esté vacía
+        assertThat(listaEmpleados).isNotNull();
+        assertThat(listaEmpleados.size()).isEqualTo(2);
     }
 
 }
